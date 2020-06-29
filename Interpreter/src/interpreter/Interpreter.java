55b0cf2c039583819bf1fa2520896a6e76bfe164
st <>
2020-06-29 23:20:27 +0300

    evalVariable

--- Interpreter/src/interpreter/Interpreter.java
+++ Interpreter/src/interpreter/Interpreter.java
@@ -21,6 +21,7 @@ public class Interpreter {
 	}
 	
 	public String ProgramBody() {
+		String exprResult = "";
 		for (String currentLine : lines) {
 			//Lexer currentLineLexer = new Lexer(currentLine);
 			//List<Token> tokenList = currentLineLexer.getTokens();
@@ -28,13 +29,33 @@ public class Interpreter {
 			   !currentLine.contains("{")) {
 				    String[] lineFragments = currentLine.split(";");
 				    for(String currentFragment : lineFragments) {
-				    	variablesDefinition(currentFragment);
+				    	variablesDefinition(currentFragment, true);
 				    }
+			}
+			else if(!currentLine.startsWith("int ") && !currentLine.startsWith("test ") && 
+					currentLine.endsWith(";") && !currentLine.contains("{")) {
+				String[] lineFragments = currentLine.split(";");
+			    for(String currentFragment : lineFragments) {
+			    	variablesDefinition(currentFragment, false);
+			    }
+			}
+			else if((currentLine.startsWith("test ") && currentLine.endsWith(";")) || 
+					(!currentLine.endsWith(";") && !currentLine.endsWith("{") && !currentLine.endsWith("}"))) {
+				if(currentLine.startsWith("test ")) {
+					
+				}
+				else if(!currentLine.contains("(") && !currentLine.contains("connect") &&
+						!currentLine.contains("disconnect") && !currentLine.contains("most-failing-test") &&
+						!currentLine.contains("most-executed-test")) {
+					exprResult = calculateVariableExpressionResult(currentLine);
+					break;
+				}
+				
 			}
 			
 		}	
 			
-		return "";
+		return exprResult;
 	}
 	
 	
@@ -43,33 +64,100 @@ public class Interpreter {
 	}	
 	
 
-	public void variablesDefinition(String lineText) {
-		String variableName = "";
-		String variableValue = "";		
-		if(lineText.contains(",")) {
+	public void variablesDefinition(String lineText, boolean defined) {
 			
+		if(lineText.contains(",")) {
+			String[] variableFragments = lineText.split(",");
+			for(String currentVariableFragment : variableFragments) {
+				int assignIndex = currentVariableFragment.indexOf("=");
+				if(assignIndex > 0) {
+					parseAssignedVariable(currentVariableFragment,assignIndex, defined);				
+				}
+				else {
+					parseUnassignedVariable(currentVariableFragment, defined);
+				}
+			}				
 		}
 		else {
 			int assignIndex = lineText.indexOf("=");
 			if(assignIndex > 0) {
-				if(lineText.startsWith("int ")) {
-					int intIndex = lineText.indexOf("int ") + 4;
-					variableName = lineText.substring(intIndex,assignIndex).trim();
-					variableValue = lineText.substring(assignIndex + 1).replace(" ","");
+				parseAssignedVariable(lineText,assignIndex, defined);				
+			}
+			else {
+				parseUnassignedVariable(lineText, defined);
+			}            
+		}	
+	}
+	
+	public void parseAssignedVariable(String assignExpression, int assignIndex, boolean defined) {
+		String variableName = "";
+		String variableValue = "";	
+		if(assignExpression.startsWith("int ")) {
+			int intIndex = assignExpression.indexOf("int ") + 4;
+			variableName = assignExpression.substring(intIndex,assignIndex).trim();
+			variableValue = assignExpression.substring(assignIndex + 1).replace(" ","");
+			if(defined) {
+				currentData.put(variableName, variableValue);
+			}
+			else {
+				if(currentData.get(variableName) != null) {
 					currentData.put(variableName, variableValue);
 				}
 				else {
-					
-				}
-				
+					currentData.put(variableName, "not defined");
+				}					
+			}
+			
+		}
+		else {
+			variableName = assignExpression.substring(0, assignIndex - 1).trim();
+			variableValue = assignExpression.substring(assignIndex + 1).trim();
+			if(defined) {
+				currentData.put(variableName, variableValue);
 			}
 			else {
-				
+				if(currentData.get(variableName) != null) {
+					currentData.put(variableName, variableValue);
+				}
+				else {
+					currentData.put(variableName, "not defined");
+				}					
 			}
-            
 		}
-		
+	}
 	
+	public void parseUnassignedVariable(String assignExpression, boolean defined) {
+		String variableName = "";
+		String variableValue = "";	
+		if(assignExpression.startsWith("int ")) {
+			int intIndex = assignExpression.indexOf("int ") + 4;
+			variableName = assignExpression.substring(intIndex).trim();			
+			if(defined) {
+				currentData.put(variableName, variableValue);
+			}
+			else {
+				if(currentData.get(variableName) != null) {
+					currentData.put(variableName, variableValue);
+				}
+				else {
+					currentData.put(variableName, "not defined");
+				}					
+			}
+		}
+		else {
+			variableName = assignExpression.trim();			
+			if(defined) {
+				currentData.put(variableName, variableValue);
+			}
+			else {
+				if(currentData.get(variableName) != null) {
+					currentData.put(variableName, variableValue);
+				}
+				else {
+					currentData.put(variableName, "not defined");
+				}					
+			}
+		}
 	}
 	
 	public void methodDefinition(String textBlock) {
@@ -80,12 +168,56 @@ public class Interpreter {
 		
 	}
 	
-	public void calculateResult() {
-		
+	public String calculateVariableExpressionResult(String evaluationText) {
+		return	evalVariable(currentData,evaluationText.replace(" ", ""));
 	}
+	
+	public String evalVariable(DataStorage dataTable, String key) {
+		if(dataTable.get(key) == null || dataTable.get(key).isEmpty()) {
+			return key + " is not initialized "; 
+		}
+		else if(dataTable.get(key) == "not defined") {
+			return key + " not defined ";
+		}
+		else if(tryParseInt(dataTable.get(key))) {
+			return dataTable.get(key);
+		}
+		else {
+			Lexer evalLexer = new Lexer(key);
+			Token t = evalLexer.getToken();
+			int identifierLength = t.getValue().length();
+			if(t.getType() == TokenType.Identifier) {
+				Token op = evalLexer.getToken();
+				switch(op.getType()) {
+					case Op_add:
+					 return	Integer.toString(Integer.parseInt(evalVariable(dataTable,t.getValue())) + 
+						Integer.parseInt(evalVariable(dataTable, key.substring(identifierLength + 1))));
+					case Op_subtract:
+						 return	Integer.toString(Integer.parseInt(evalVariable(dataTable,t.getValue())) - 
+							Integer.parseInt(evalVariable(dataTable, key.substring(identifierLength + 1))));
+					default:
+						return "";
+				
+				}				
+			}
+			else {
+				return "";
+			}
+		}
+	}
+	
 	
 	public void connectIPBlock() {
 		
+	}
+	
+	boolean tryParseInt(String value) {  
+	     try {  
+	         Integer.parseInt(value);  
+	         return true;  
+	      } catch (NumberFormatException e) {  
+	         return false;  
+	      }  
 	}
 	
 }
